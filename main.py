b8d3d58cb923b9cf51e79254f392374d1b914fc0
Lee Baldwin <aplusdesigners@gmail.com>
2019-07-17 18:42:20 -0400

    Update main.py
    wrote generating function

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 #to generate random string
 import random
 
-chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-pwdlen = int(input('Desired Password Length: '))
-pwdnums = int(input('Number of Passwords to Generate: '))
-
+#by using a large number of characters
+#we can create a much harder password
+strchars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_*+=!@#$%^&'
+len = int(input('Desired Password Length: '))
+nums = int(input('Number of Passwords to Generate: '))
+pwdlen = int(len)
+pwdnums = int(nums)
+"""
 for pn in range(pwdnums):
     password = ''
     for pl in range(pwdlen):
         password += random.choice(chars)
     print(password)
+"""
+def pwdgen(pwdnums = 1, pwdlen = 8, chars = strchars):
+#We will generate passwords with either parameters set by the user input
+#or with default settings
+    
